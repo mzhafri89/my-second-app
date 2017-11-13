aa8563c7e6f81b0e38c4e1bb86e136dd4b332ede
Mohd Zhafri <mohdzhafri@Mohds-MacBook-Pro.local>
2017-11-13 23:16:37 +0800

    Created component

---
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-details',
+  templateUrl: './recipe-details.component.html',
+  styleUrls: ['./recipe-details.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RecipeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
